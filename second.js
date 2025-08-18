c83b0331a31def59437ec2933edb60160d2644c8
muskan181103 <chaurasiamuskan675@gmail.com>
2025-08-18 19:59:23 +0530

    while loop

---
+++ second.js
@@ -0,0 +1,26 @@
+// let st = 1
+
+// while (st<=10){
+//     console.log(st);
+//     st++
+// }
+
+
+// let num = 1
+// let sum = 0
+// while(num<=10){
+//     sum = num + sum
+//     console.log(sum);
+//     num++
+    
+// }
+
+let num = 1
+
+ while (num<=15){
+    if (num%2==0){
+       console.log(num); 
+    }
+    
+    num++
+ }
