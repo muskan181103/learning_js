8ac5fec704c4aabb7c6916ead6c69f609d6ce088
muskan181103 <chaurasiamuskan675@gmail.com>
2025-08-19 14:36:23 +0530

    if else

---
+++ first.js
@@ -0,0 +1,44 @@
+
+// let age = parseInt(prompt("Enter your ege "))
+// if (age >= 18){
+//  let n = parseInt(prompt("Press 1 for Indian"))
+//  if (n==1){
+//     console.log("Eligible for voting")
+//  }
+//  else{
+//     console.log("not Eligible");
+    
+//  }
+// }
+// else {
+//     console.log("You are Under age")
+// }
+//=============================================//
+
+//loop problem//
+//do while loop//
+
+// let start =1
+// do{
+//     console.log("Muskan");
+//     start++
+    
+// }
+// while (start<11)
+
+// write a program to print a table of any number//
+
+// let n = parseInt(prompt("Enter any number"))
+// let x = 1
+// do{
+//     console.log(n*x);
+//     x++
+// }
+// while(x<11)
+
+
+//  do{
+//     let n = parseInt(prompt("Enter a number"))
+   
+//     }
+//  while(n!=0)
