f0ce3ab4d96ac556f8150e2218d66fa214ffa829
muskan181103 <chaurasiamuskan675@gmail.com>
2025-08-24 20:49:53 +0530

    funtion

---
+++ function.js
@@ -0,0 +1,46 @@
+//Add()
+/*
+ no argument and no return value
+ with argument  and no return
+ with argument and with return
+ no argument and return value
+*/
+// type 1
+
+// function Add(){
+//     let a =12
+//     let b = 90
+//     console.log(a+b);
+    
+// }
+// type 2
+
+// function Add( num){
+//      console.log( num + num);
+     
+// }
+// Add(10)/
+
+
+// function Area(r){
+//     let area = 3.14*r*r
+//     console.log("Area of circle :"+ area);
+// }
+// let r= parseInt(prompt("Enter the Radius:"))
+// Area( r)
+
+function Evenodd(num){
+if(num%2==0){
+    console.log("Number is Odd"+ num);
+    
+}
+else{
+    console.log("Number is even "+ num);
+    
+}
+}
+let num = parseInt(prompt("Enter any number"))
+Evenodd(num)
+
+
+
