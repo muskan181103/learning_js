eb5ebf73ef342a031de45a7905f514d6689b2a01
muskan181103 <chaurasiamuskan675@gmail.com>
2025-08-24 20:43:07 +0530

    javascript

--- for1.js
+++ for1.js
@@ -1,10 +1,92 @@
 
-
-
-for(let r = 1; r<=5; r++){
-    for( let c = 1; c<=r; c++){
-        document.writeln("*")
+// for(let r = 1; r<=5; r++){
+//     for( let c = 1; c<=r; c++){
+//         document.writeln("*")
         
-    }
-    document.writeln("<br>")
-}
+//     }
+//     document.writeln("<br>")
+// }
+
+//=======================================================//
+
+// let user = prompt("Please enter here:");
+// if (user === "Hello") {
+ 
+//   console.log("Hello World");
+// }
+
+//============================================================//
+
+// let age = parseInt(prompt("Enter the Age:"));
+//  if (age > 23) {
+//     console.log("Your age is greater than 23.");
+    
+//   } else {
+//     console.log("Your age is not greater than 23.");
+    
+//   }
+//================================================//
+
+// let num = parseInt(prompt("Enter the number"));
+// if(num % 2==0){
+//     let radius = num
+//     let area = 3.14 * radius * radius
+//     console.log("Number is odd :"+ num + "Area of circle:" + area );
+//    }
+//    else{
+//      let sq = num * num 
+//      console.log(sq);
+     
+//    }
+//========================================================//
+
+// let Hindi = parseInt(prompt("Hindi"));
+// let English = parseInt(prompt("English"));
+// let Maths = parseInt(prompt("Maths"));
+// let Science = parseInt(prompt("Science"));
+// let Social = parseInt(prompt("Social"));
+
+// let totalMarks = Hindi + English + Maths + Science + Social;
+
+// let maxMarks = 500;
+
+// let percentage = (totalMarks/ maxMarks)* 100;
+
+// let grade;
+// if (percentage >= 90) {
+//     grade = "A";
+// } else if (percentage >= 80) {
+//     grade = "B";
+// } else if (percentage >= 70) {
+//     grade = "C";
+// } else if (percentage >= 60) {
+//     grade = "D";
+// } else {
+//     grade = "F";
+// }
+//  console.log("Percentage: "+ percentage + "%");
+//  console.log("Grade: "+ grade);
+ 
+ //==============================================================//
+
+//  let num1 = parseInt(prompt("Enter the first number:"));
+
+//  let num2 = parseInt(prompt("Enter the second number:"));
+
+//  let num3 = parseInt(prompt("Enter the third number:"));
+ 
+//  if(num1||num2||num3>= 0){
+ 
+//     if(num1 ||num2 ||num3 % 2 == 0){
+//          let sum = num1 + num2 + num3;
+//          console.log("The  number is positive and even.");
+//          console.log("Sum of all inputs:", sum);
+//     } else {
+//     console.log("The first number is positive and odd.");
+    
+//   }
+//  }else {
+//   let product = num1 * num2 * num3;
+//   console.log("The first number is negative.");
+//   console.log("Product of all inputs:", product);
+// }
